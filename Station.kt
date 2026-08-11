2664fde87d11086359c6ae27f6d25ccc96fe042d
EcoByte3R <ecobyte3r@outlook.com>
2026-08-11 13:07:27 +0100

    Update Station.kt

--- Station.kt
+++ Station.kt
@@ -43,7 +43,25 @@ fun stationsForLine(lineKey: String): List<Station> {
 }
 
 val allStations = listOf(
-    // 
+    // A (20)
+
+    // B ()
+    Station("", ,listOf(R.string.station_)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
+    Station("", ,listOf(R.string.)
     Station("blackhorse_road", R.string.station_blackhorse_road, listOf(R.string.zone_3)),
     Station("brixton", R.string.station_brixton, listOf(R.string.zone_2)),
 
@@ -52,14 +70,34 @@ val allStations = listOf(
     Station("finsbury_park", R.string.station_finsbury_park, listOf(R.string.zone_2)),
     Station("green_park", R.string.station_green_park, listOf(R.string.zone_1)),
     Station("highbury_and_islington", R.string.station_highbury_and_islington, listOf(R.string.zone_2)),
+
+    // I (5)
+    Station("ickenham", ,listOf(R.string.station_ickenham)
+    Station("ilford", ,listOf(R.string.station_ilford)
+    Station("imperial_wharf", ,listOf(R.string.station_imperial_wharf)
+    Station("island_gardens", ,listOf(R.string.station_island_gardens)
+    Station("iver", ,listOf(R.string.station_iver)
+
     Station("kings_cross_st_pancras", R.string.station_kings_cross_st_pancras, listOf(R.string.zone_1)),
     Station("oxford_circus", R.string.station_oxford_circus, listOf(R.string.zone_1)),
     Station("pimlico", R.string.station_pimlico, listOf(R.string.zone_1)),
     Station("seven_sisters", R.string.station_seven_sisters, listOf(R.string.zone_3)),
     Station("stockwell", R.string.station_stockwell, listOf(R.string.zone_2)),
     Station("tottenham_hale", R.string.station_tottenham_hale, listOf(R.string.zone_3)),
+
+    // U (6)
+    Station("upminster", ,listOf(R.string.station_upminster)
+    Station("upminster_bridge", ,listOf(R.string.station_upminster_bridge)
+    Station("upney", ,listOf(R.string.station_upney)
+    Station("upper_holloway", ,listOf(R.string.station_upper_holloway)
+    Station("upton_park", ,listOf(R.string.station_upton_park)
+    Station("uxbridge", ,listOf(R.string.station_uxbridge)
+
+    // V (2)
     Station("vauxhall", R.string.station_vauxhall, listOf(R.string.zone_1, R.string.zone_2)),
     Station("victoria", R.string.station_victoria, listOf(R.string.zone_1)),
+
+  
     Station("walthamstow_central", R.string.station_walthamstow_central, listOf(R.string.zone_3)),
     Station("warren_street", R.string.station_warren_street, listOf(R.string.zone_1))
 )
